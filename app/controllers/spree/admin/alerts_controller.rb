2261a78a8449dc29076c06093a58a41a08d656cc
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2014-01-01 12:54:31 +0100

    more modifications

--- app/controllers/spree/admin/alerts_controller.rb
+++ app/controllers/spree/admin/alerts_controller.rb
@@ -2,7 +2,7 @@ module  Spree
   module Admin
    class AlertsController < ResourceController
     def index
-      @alerts = Spree::Alert.all.page(params[:page]).per(10)
+      @alerts = Spree::Alert.page(params[:page]).per(10)
       respond_to do |format|
         format.html # index.html.erb
         format.js 
